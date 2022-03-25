fb6d5d36e6e6f78d6d387ba377c9461b6fa95b1e
Gracjan Winiarski <Gracjan.Winiarski@microsoft.wsei.edu.pl>
2022-03-25 12:53:57 +0100

    Lab 03

---
+++ lab_03/lab_03/lab_03/Logger/SocketLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_03.Logger
+{
+    class SocketLogger : ClientSocket
+    {
+        private ClientSocket clientSocket;
+
+        public SocketLogger(string host, int port) : base(host, port)
+        {
+
+        }
+
+        ~SocketLogger()
+        {
+            this.Dispose(false);
+        }
+
+        public void Log(params string[] messages )
+        {
+
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
